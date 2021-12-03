e7a7da7225a8dad00aaa01c0f990c08987e144a0
NaotoshiFujita <fujita.naotoshi@gmail.com>
2021-12-03 20:20:27 +0900

    Update the Splide version.

--- dist/js/splide-extension-auto-scroll.cjs.js
+++ dist/js/splide-extension-auto-scroll.cjs.js
@@ -1,6 +1,6 @@
 /*!
  * @splidejs/splide-extension-auto-scroll
- * Version  : 0.3.2
+ * Version  : 0.3.3
  * License  : MIT
  * Copyright: 2021 Naotoshi Fujita
  */
@@ -349,7 +349,7 @@ function AutoScroll(Splide3, Components2, options) {
 }
 /*!
  * Splide.js
- * Version  : 3.6.8
+ * Version  : 3.6.9
  * License  : MIT
  * Copyright: 2021 Naotoshi Fujita
  */
--- dist/js/splide-extension-auto-scroll.js
+++ dist/js/splide-extension-auto-scroll.js
@@ -1,6 +1,6 @@
 /*!
  * @splidejs/splide-extension-auto-scroll
- * Version  : 0.3.2
+ * Version  : 0.3.3
  * License  : MIT
  * Copyright: 2021 Naotoshi Fujita
  */
@@ -422,7 +422,7 @@
   }
   /*!
    * Splide.js
-   * Version  : 3.6.8
+   * Version  : 3.6.9
    * License  : MIT
    * Copyright: 2021 Naotoshi Fujita
    */
